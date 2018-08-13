becc0e00d9df04c31793b38e4688fa03c431ff16
Admin <admin@mac-admin.local>
2018-08-13 17:46:29 +0300

    AuthVC

--- CoffeeshopManager/AuthViewController.swift
+++ CoffeeshopManager/AuthViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class AuthViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,11 +17,13 @@ class ViewController: UIViewController {
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loginTapped(_ sender: Any) {
+        
     }
-
-
+    
+    @IBAction func SignUpTapped(_ sender: Any) {
+    }
+    
+    
 }
 
